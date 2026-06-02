67d395062d0bc72640b8f3b5b4494be832bea930
velko94 <vrizov5@gmail.com>
2026-06-02 13:11:55 +0300

    changed the hashing from hashing the api key to hashing the password cleared a bit the check password function

--- irrigation-api/security.py
+++ irrigation-api/security.py
@@ -1,29 +1,23 @@
 import os
-
 import bcrypt
-
 global pwd_bytes, hashed
-from database import register_user
 
 
-def get_password_encode():
+
+def get_password_encode(raw_password:str):
     global pwd_bytes, hashed
     api_key_string = os.getenv("IRRIGATION_API_KEY","КОЙ ПОЛИВА И КОГА")
-    pwd_bytes = api_key_string.encode('utf-8')
+    pwd_bytes = raw_password.encode('utf-8')
     hashed = bcrypt.hashpw(pwd_bytes, bcrypt.gensalt())
-
+    return hashed
 
 def get_password_decode():
     decode_pwd = hashed.decode('utf-8')
     return decode_pwd  # - returns the decoded password
 
 
-def check_pwd(username_input: str):
-    if bcrypt.checkpw(pwd_bytes, hashed):
-        print("it matches")
-        db_hash_str = get_password_decode()
-        status = register_user(username_input, db_hash_str)
-        return status
-    else:
-        print("Passwords don't match")
-        return "Wrong password"
+def check_pwd(raw_password: str, hashed_pwd_from_db: str ):
+        return bcrypt.checkpw(raw_password.encode('utf-8'), hashed_pwd_from_db.encode('utf-8'))
+
+
+
