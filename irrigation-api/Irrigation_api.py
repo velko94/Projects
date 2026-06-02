a9f09673fed0fb356ad23ba041bbaf779409f7db
velko94 <vrizov5@gmail.com>
2026-06-02 16:11:17 +0300

    fixing the identations on the field validators and adding the custom messages for the input validation

--- irrigation-api/Irrigation_api.py
+++ irrigation-api/Irrigation_api.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, HTTPException, Security, Depends
 from fastapi.security import APIKeyHeader
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, Field
+import re
 import os
 from mangum import Mangum
 from datetime import datetime
+
+from pydantic_core import PydanticCustomError
+
 from database import engine, Base
 from users import register_user
-
 
 Base.metadata.create_all(bind=engine)
 
@@ -24,9 +27,37 @@ def verifiy_api_key(header_key: str = Security(api_key_header)):
         raise HTTPException(status_code=401, detail="Wrong api key")
     return header_key
 
+
 class UserRegister(BaseModel):
-    username: str
-    password: str
+    username: str = Field(...)
+    password: str = Field(..., min_length=5)
+
+    @field_validator('username', mode='before')
+    def validate_username(cls, v):
+        if not isinstance(v, str):
+            raise PydanticCustomError('Потребителското име трябва да е текст')
+
+        username_clean = v.strip()
+
+        if not username_clean:
+            raise PydanticCustomError('empty_error','Потребителското неможе да е празно')
+        if len(username_clean) < 3:
+            raise PydanticCustomError('length_error','Потребителското име трябва да има поне 3 символа')
+        if not v.strip():
+            raise PydanticCustomError('value_error','Полето не може да бъде празно или да съдържа само интервали!')
+        if not re.match(r'^[a-zA-Z0-9_а-яА-Я]+$', username_clean):
+            raise PydanticCustomError('value_error',"Потребителското име може да съдържа само букви цифри и долни черти")
+        return username_clean
+
+
+    @field_validator('password')
+
+    def validate_password(cls, v):
+      pwd_clean = v.strip()
+      if len(pwd_clean) < 5:
+        raise PydanticCustomError('length_error','Паролата трябва да е поне 5 символа')
+      return pwd_clean
+
 
 Zones_db = {
     1: {"name": "Домати и пипер", "status": "off", "last_watered": "Never"},
@@ -69,6 +100,5 @@ def receive_telemetry(data: SensorData, token: str = Depends(verifiy_api_key)):
 
 
 @app.post("/api/v1/register")
-def register_user_main(user_data:UserRegister):
-    return register_user(user_data.username,user_data.password)
-
+def register_user_main(user_data: UserRegister):
+    return register_user(user_data.username, user_data.password)
